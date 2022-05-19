9f9146c81568464d3c4045ee3d2cdc0ddb306f11
ana gabriela <anagabrieladev@gmail.com>
2022-05-19 04:44:05 -0300

    refactor: add/delete imports

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express'; 
+import cors from 'cors';
 import { routes } from './routes';
 
 const app = express();
 
+app.use(cors());//to tell who can use this server
 app.use(express.json());
 app.use(routes)
 
